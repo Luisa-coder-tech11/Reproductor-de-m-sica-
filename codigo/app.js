011d5d15710b9613ec11a0e303868454ddaff3b1
Luisa-coder-tech11 <luisa.munom1@udea.edu.co>
2025-07-28 17:16:51 -0500

    Inicio Reproductor musical

---
+++ codigo/app.js
@@ -0,0 +1,11 @@
+angular.module("Reproductor",[])
+.controller("ControladorReproductor",function($scope, $http){
+
+    $scope.artistas=[];
+    
+    //cargue de los datos (solo posible mediante servicios)
+    $http.get("datos/Musiteca.json")
+    .then(function(datos){
+        $scope.artistas=datos.data.Artistas;
+    });
+});
