673f1d3f304798543c933a0b073fca1dc7baa55f
Luisa-coder-tech11 <luisa.munom1@udea.edu.co>
2025-07-29 17:32:31 -0500

    lo que fue fue

--- codigo/app.js
+++ codigo/app.js
@@ -1,14 +1,50 @@
-angular.module("Reproductor",[])
-.controller("ControladorReproductor",function($scope, $http){
+angular.module("Reproductor", [])
+    .controller("ControladorReproductor", function ($scope, $http, $interval) {
 
-    $scope.artistas=[];
-    
-    //cargue de los datos (solo posible mediante servicios)
-    $http.get("datos/Musiteca.json")
-    .then(function(datos){
-        $scope.artistas=datos.data.Artistas;
+        $scope.artistas = [];
+        $scope.canciones = [];
+        $scope.artistaSeleccionado = "";
+        $scope.cancionSeleccionada = "";
+        $scope.rutaCancion = "";
+        $scope.reproductor = new Audio();
+        $scope.duracion = 0;
+        $scope.tiempoActual = 0;
+
+        //cargue de los datos (solo posible mediante servicio)
+        $http.get("datos/Musiteca.json")
+            .then(function (datos) {
+                $scope.artistas = datos.data.Artistas;
+            });
+
+        $scope.seleccionarArtista = function (artista) {
+            $scope.canciones = artista.Canciones;
+            $scope.artistaSeleccionado = artista.Nombre;
+        }
+
+        $scope.seleccionarCancion = function (cancion) {
+            $scope.cancionSeleccionada = cancion.Titulo + " - " + $scope.artistaSeleccionado;
+            $scope.rutaCancion = "canciones/" + $scope.artistaSeleccionado + " - " + cancion.Titulo + ".mp3";
+            $scope.reproductor.src = $scope.rutaCancion;
+            $scope.duracion = $scope.reproductor.duration;
+            $scope.tiempoActual = 0;
+        }
+
+        $scope.reproducir = function () {
+            $scope.reproductor.play();
+        }
+
+        $scope.pausar = function () {
+            $scope.reproductor.pause();
+        }
+
+        $scope.ubicarTiempoCancion = function () {
+            $scope.reproductor.currentTime = $scope.tiempoActual;
+        }
+
+        $interval(function () {
+            if (!$scope.reproductor.paused) {
+                $scope.tiempoActual = $scope.reproductor.currentTime;
+            }
+        }, 500, 0, true);
+
     });
-    $scope.seleccionarArtistas=function (artista){
-        $scope.canciones=artista.canciones;
-    }
-});
